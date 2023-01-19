c29ecd15f2fa921db16413b9434dd3553e2f9bc9
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-19 20:18:31 +0530

    Added code to display the various DZone categories from where we can obtain technical news feed.

--- app/src/main/java/com/technewsfeed/DZoneCategoriesAppPageActivity.java
+++ app/src/main/java/com/technewsfeed/DZoneCategoriesAppPageActivity.java
@@ -2,12 +2,40 @@ package com.technewsfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DZoneCategoriesAppPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dzone_categories_app_page);
+
+        Context context = this;
+
+        String[] categoriesResourceStrings = Utility.getResourceStringsFromIds(
+            context,
+            R.string.text_dzone_category_agile,
+            R.string.text_dzone_category_ai,
+            R.string.text_dzone_category_big_data,
+            R.string.text_dzone_category_cloud,
+            R.string.text_dzone_category_database,
+            R.string.text_dzone_category_devops,
+            R.string.text_dzone_category_integration,
+            R.string.text_dzone_category_iot,
+            R.string.text_dzone_category_java,
+            R.string.text_dzone_category_microservices,
+            R.string.text_dzone_category_open_source,
+            R.string.text_dzone_category_performance,
+            R.string.text_dzone_category_security,
+            R.string.text_dzone_category_web_dev
+        );
+
+        ListView dZoneCategoriesListView = findViewById(R.id.dzone_categories_list_view);
+
+        ArrayAdapter<String> dZoneCategoriesListViewArrayAdapter = new ArrayAdapter<>(context, R.layout.item_dzone_categories_list, categoriesResourceStrings);
+        dZoneCategoriesListView.setAdapter(dZoneCategoriesListViewArrayAdapter);
     }
 }
