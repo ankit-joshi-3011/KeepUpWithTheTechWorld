150defff1c2e892edd6aa114060f66a94abde3b8
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-19 12:55:43 +0530

    Added an activity that will show all the DZone technical news categories.

---
+++ app/src/main/java/com/technewsfeed/DZoneCategoriesAppPageActivity.java
@@ -0,0 +1,14 @@
+package com.technewsfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DZoneCategoriesAppPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dzone_categories_app_page);
+    }
+}
