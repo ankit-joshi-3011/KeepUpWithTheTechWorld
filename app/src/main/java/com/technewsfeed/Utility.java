e4ec3d0d1d7ec9991b5a0474eb2541121ca92cea
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-19 17:50:09 +0530

    - Created a new class Utility. This class will hold code, that is common across multiple other classes. - Added a method to this Utility class called getResourceStringFromIds.

---
+++ app/src/main/java/com/technewsfeed/Utility.java
@@ -0,0 +1,15 @@
+package com.technewsfeed;
+
+import android.content.Context;
+
+public class Utility {
+    public static String[] getResourceStringsFromIds(Context context, int... resourceIds) {
+        String[] resourceStrings = new String[resourceIds.length];
+
+        for (int index = 0; index < resourceIds.length; index++) {
+            resourceStrings[index] = context.getString(resourceIds[index]);
+        }
+
+        return resourceStrings;
+    }
+}
