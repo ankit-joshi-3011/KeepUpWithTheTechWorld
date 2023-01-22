411209cd83605e5223f8c602ab9b66bd7c5150bb
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-22 13:47:46 +0530

    Added code to launch the activity that shows the DZone Categories when a user clicks on the DZone technical news feed.

--- app/src/main/java/com/technewsfeed/FirstAppPageActivity.java
+++ app/src/main/java/com/technewsfeed/FirstAppPageActivity.java
@@ -1,6 +1,7 @@
 package com.technewsfeed;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,11 +16,21 @@ public class FirstAppPageActivity extends AppCompatActivity {
 
         Context context = this;
 
-        String[] newsFeedSourceResourceStrings = Utility.getResourceStringsFromIds(context, R.string.text_dzone_news_feed_source_item);
+        int[] newsFeedSourceResourceIds = new int[] { R.string.text_dzone_news_feed_source_item };
+        String[] newsFeedSourceResourceStrings = Utility.getResourceStringsFromIds(context, newsFeedSourceResourceIds);
 
         ListView newsFeedSourcesListView = findViewById(R.id.news_feed_sources_list_view);
 
         ArrayAdapter<String> newsFeedSourcesListViewAdapter = new ArrayAdapter<>(context, R.layout.item_news_feed_sources_list, newsFeedSourceResourceStrings);
         newsFeedSourcesListView.setAdapter(newsFeedSourcesListViewAdapter);
+
+        newsFeedSourcesListView.setOnItemClickListener((parent, view, position, id) -> {
+            int newsFeedSourceResourceClicked = newsFeedSourceResourceIds[position];
+
+            if (newsFeedSourceResourceClicked == R.string.text_dzone_news_feed_source_item) {
+                Intent dZoneCategoriesAppPageActivityIntent = new Intent(context, DZoneCategoriesAppPageActivity.class);
+                context.startActivity(dZoneCategoriesAppPageActivityIntent);
+            }
+        });
     }
 }
