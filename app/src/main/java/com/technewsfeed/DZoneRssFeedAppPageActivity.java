bd47f93b0cd089c3e9e88bd6b2e8a0747607b786
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-22 21:05:37 +0530

    Added a new class called WebViewClientExtended so that we can ensure that any URL clicked from within the WebView is loaded in the WebView itself.

--- app/src/main/java/com/technewsfeed/DZoneRssFeedAppPageActivity.java
+++ app/src/main/java/com/technewsfeed/DZoneRssFeedAppPageActivity.java
@@ -1,7 +1,9 @@
 package com.technewsfeed;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +14,17 @@ public class DZoneRssFeedAppPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dzone_rss_feed_app_page);
 
         WebView dZoneRssFeedWebView = findViewById(R.id.dzone_rss_feed_web_view);
+        dZoneRssFeedWebView.setWebViewClient(new WebViewClientExtended());
 
         String dZoneRssFeedUrl = getIntent().getStringExtra(Constants.DZONE_RSS_FEED_URL_EXTRA_KEY);
         dZoneRssFeedWebView.loadUrl(dZoneRssFeedUrl);
+    }
+
+    static class WebViewClientExtended extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
     }
 }
