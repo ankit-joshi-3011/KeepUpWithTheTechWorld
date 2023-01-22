0f2a9e2fdf0bd4102929405d93516320de33a0ed
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-22 14:01:29 +0530

    Added an activity that will open a selected DZone Category RSS Feed URL in a WebView.

---
+++ app/src/main/java/com/technewsfeed/DZoneRssFeedAppPageActivity.java
@@ -0,0 +1,14 @@
+package com.technewsfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DZoneRssFeedAppPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dzone_rss_feed_app_page);
+    }
+}
