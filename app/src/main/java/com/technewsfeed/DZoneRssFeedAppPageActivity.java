e462748e400531bf1b9d1e530c2d771ef8f3f993
Ankit Joshi <ankit.joshi3011@gmail.com>
2023-01-22 15:17:24 +0530

    Added code to display DZone RSS Feed URL in the WebView.

--- app/src/main/java/com/technewsfeed/DZoneRssFeedAppPageActivity.java
+++ app/src/main/java/com/technewsfeed/DZoneRssFeedAppPageActivity.java
@@ -1,8 +1,9 @@
 package com.technewsfeed;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DZoneRssFeedAppPageActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class DZoneRssFeedAppPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dzone_rss_feed_app_page);
+
+        WebView dZoneRssFeedWebView = findViewById(R.id.dzone_rss_feed_web_view);
+
+        String dZoneRssFeedUrl = getIntent().getStringExtra(Constants.DZONE_RSS_FEED_URL_EXTRA_KEY);
+        dZoneRssFeedWebView.loadUrl(dZoneRssFeedUrl);
     }
 }
